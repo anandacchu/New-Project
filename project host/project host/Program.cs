8ed8c354647cd18fd2d5eb27900c78b3d6e2c504
anand acchu <you@example.com>
2022-08-02 09:12:18 +0530

    [anand]Add:UC2 demo2

--- project host/project host/Program.cs
+++ project host/project host/Program.cs
@@ -7,6 +7,8 @@ namespace FirstProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
+
     }
 }
